0566cf7fdc8fde23f69c213ada94a9906974b44a
yamadapc <tacla.yamada@gmail.com>
2016-08-24 14:19:12 -0300

    Add option parsing to conffmt
    This closes #1

--- bin/ConfFmt.hs
+++ bin/ConfFmt.hs
@@ -1,19 +1,52 @@
-import qualified Data.Conf          as Conf
-import qualified Data.Text.IO       as Text (readFile)
-import           System.Environment (getArgs)
-import           System.Exit        (exitFailure)
+{-# LANGUAGE RecordWildCards #-}
+import           Control.Monad
+import qualified Data.Conf                 as Conf
+import qualified Data.Text.IO              as Text (hGetContents, readFile)
+import           Options.Applicative
+import           Options.Applicative.Types
+import           System.Environment        (getArgs)
+import           System.Exit               (exitFailure)
 import           System.IO
-import qualified Text.Megaparsec    as Megaparsec
+import qualified Text.Megaparsec           as Megaparsec
+
+data ConfFmtOptions
+    = ConfFmtOptions { cfoInplace :: Bool
+                     , cfoOutput  :: Maybe FilePath
+                     , cfoInput   :: Maybe FilePath
+                     }
+
+options :: Parser ConfFmtOptions
+options = ConfFmtOptions
+    <$> cfoInplaceAP
+    <*> cfoOutputAP
+    <*> cfoInputAP
+
+cfoInputAP = optional (argument str
+    (metavar "INPUTFILE"
+    <> help "The input file (omission will cause `conffmt` to use stdin)"))
+
+cfoOutputAP = optional (option str (short 'o' <> long "output"))
+cfoInplaceAP = switch (long "inplace"
+    <> short 'i'
+    <> help "Format the FILE inplace, replacing it's contents")
 
 main :: IO ()
 main = do
-    as <- getArgs
-    case as of
-        (filePath:_) -> do
-            contents <- Text.readFile filePath
-            case Megaparsec.parse Conf.conf filePath contents of
-                Left err -> hPrint stderr err
-                Right ast -> print (Conf.pPrintConf ast)
-        _ -> do
-            hPutStrLn stderr "Usage: conffmt <conf-file>"
-            exitFailure
+    ConfFmtOptions{..} <- execParser (info (helper <*> options)
+                                   ( fullDesc
+                                     <> progDesc "Format a .conf FILE"
+                                     <> header "conffmt - Conf formatter using Haskell language-conf"
+                                   ))
+    (contents, fp) <- case cfoInput of
+        Just fp ->
+            (,) <$> Text.readFile fp <*> pure fp
+        Nothing ->
+            (,) <$> Text.hGetContents stdin <*> pure "<stdin>"
+    case Megaparsec.parse Conf.conf fp contents of
+        Left err -> hPrint stderr err
+        Right ast -> do
+            let output = show (Conf.pPrintConf ast)
+            case (cfoInplace, cfoOutput) of
+                (_, Just outputFp) -> writeFile outputFp output
+                (True, _) -> writeFile fp output
+                _ -> putStr output
